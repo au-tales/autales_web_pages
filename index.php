40ee00a26782366856348457d18ade1e67a26657
MUAZZAM <muazzam.younus925@gmail.com>
2021-03-25 10:11:22 +0500

    Update Homepage Basic Section Setup

--- index.php
+++ index.php
@@ -3,9 +3,61 @@
 <?php require "components/partials/header_section.php" ?>
 <!--    Header Ends   -->
 
+<!--<div class="container-fluid">-->
+<!--    <div class="row">-->
+<!--        <div class="col-12 col-lg-1"></div>-->
+<!--        <div class="col-12 col-lg-10"></div>-->
+<!--        <div class="col-12 col-lg-1"></div>-->
+<!--    </div>-->
+<!--</div>-->
+
 <!--    Main Starts   -->
 
 <main class="page-home">
+
+    <!--  Banner Section  -->
+    <section class="banner-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-lg-1"></div>
+                <div class="col-12 col-lg-10">
+                    <div>
+
+                    </div>
+                </div>
+                <div class="col-12 col-lg-1"></div>
+            </div>
+        </div>
+    </section>
+    <!--  /Banner Section  -->
+
+    <!--  Logo Slider Section  -->
+    <section class="logo-section ">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-lg-1"></div>
+                <div class="col-12 col-lg-10">
+                    <div>
+
+                    </div>
+                </div>
+                <div class="col-12 col-lg-1"></div>
+            </div>
+        </div>
+    </section>
+    <!--  /Logo Slider Section  -->
+
+    <!--  Getting Started Section  -->
+    <!--  /Getting Started Section  -->
+
+    <!--  Mobile Categories Section  -->
+    <!--  /Mobile Categories Section  -->
+
+    <!--  Mobile Reviews Video Section  -->
+    <!--  /Mobile Reviews Video Section  -->
+
+    <!--  Join Hands Section  -->
+    <!--  /Join Hands Video Section  -->
 
 </main>
 
