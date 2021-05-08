ae37fc34bc01362d330873e5ee4d08d3a4b5d448
MUAZZAM <muazzam.younus925@gmail.com>
2021-05-09 02:08:42 +0500

    Footer & Banner Updates

--- components/partials/footer_section.php
+++ components/partials/footer_section.php
@@ -1,24 +1,251 @@
 <footer id="main-footer" class="background-tertiary">
 
-    <div class="footer-top">
+    <div class="footer-top section-padding2">
 
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
-                <div class="col-12 col-lg-1"></div>
-                <div class="col-12 col-lg-10">
-                    <h1>Hellooo</h1>
+                <div class="col-12">
+                    <div class="row no-gutters">
+                        <div class="col-12 col-lg-6 col-xl-3">
+                            <div class="footer-widget">
+                                <div class="widget-title mgb-3 mgb-lg-4">
+                                    <h4 class="font-weight-bold">Community</h4>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="list-unstyled footer-menu">
+                                        <li>
+                                            <a href="#">
+                                                Events
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Blog
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Forum
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Community Standards
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Podcast
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Affiliates
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Invite a Friend
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Become a Seller
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Fiverr Elevate
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Exclusive Benefits
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 col-xl-3">
+                            <div class="footer-widget">
+                                <div class="widget-title mgb-3 mgb-lg-4">
+                                    <h4 class="font-weight-bold">Categorie</h4>
+                                </div>
+                                <div class="widget-content">
+                                    <ul class="list-unstyled footer-menu">
+                                        <li>
+                                            <a href="#">
+                                                Graphics & Design
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Digital Marketing
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Writing & Translation
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Video & Animation
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Music & Audio
+                                            </a>
+                                        </li>
+
+                                        <li>
+                                            <a href="#">
+                                                Programming & Tech
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Business
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Lifestyle
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Sitemap
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 col-xl-6">
+                            <div class="row no-gutters">
+                                <div class="col-12 col-lg-6">
+                                    <div class="footer-widget">
+                                        <div class="widget-title mgb-3 mgb-lg-4">
+                                            <h4 class="font-weight-bold">Main menu</h4>
+                                        </div>
+                                        <div class="widget-content">
+                                            <ul class="list-unstyled footer-menu">
+                                                <li>
+                                                    <a href="#">
+                                                        Home
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        About Us
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Portfolio
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Terms &amp; Conditions
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Privacy Policy
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Sitemap
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="footer-widget">
+                                        <div class="widget-title mgb-3 mgb-lg-4">
+                                            <h4 class="font-weight-bold">About</h4>
+                                        </div>
+                                        <div class="widget-content">
+                                            <ul class="list-unstyled footer-menu">
+                                                <li>
+                                                    <a href="#">
+                                                        Careers
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Press & News
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Partnerships
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Privacy Policy
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Terms of Service
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Intellectual Property Claims
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        Investor Relations
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row no-gutters ">
+                                <div class="col-12 col-lg-10">
+                                    <div class="footer-widget">
+                                        <div class="widget-content">
+                                            <div class="newsletter-form">
+                                                <form action="" class="form-inline">
+                                                    <div class="form-group mgb-0">
+                                                        <label for="subscribe_email" class="sr-only"></label>
+                                                        <input type="email" name="subscribe_email" id="subscribe_email"
+                                                               placeholder="Your email address">
+                                                    </div>
+                                                    <button type="submit" name="subscribe_submit" id="subscribe_submit">
+                                                        <img class="img-fluid"
+                                                             src="assets/images/search.png" alt="search"></button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-12 col-lg-1"></div>
             </div>
         </div>
 
     </div>
 
     <div class="copyright-area">
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
-                <div class="col-12 col-lg-1"></div>
-                <div class="col-12 col-lg-10">
+                <div class="col-12">
                     <div class="social-wrap">
                         <ul class="list-unstyled list-social d-flex align-items-center justify-content-center">
                             <li>
@@ -66,7 +293,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-lg-1"></div>
             </div>
         </div>
     </div>
